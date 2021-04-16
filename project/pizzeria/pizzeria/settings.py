50f6d4b365f4c97f7de365cf8a8385c480efaf86
Ishaan Sharma <56833627+ishaansharma7@users.noreply.github.com>
2021-04-16 23:11:30 +0530

    Update settings.py

--- project/pizzeria/pizzeria/settings.py
+++ project/pizzeria/pizzeria/settings.py
@@ -76,6 +76,10 @@ WSGI_APPLICATION = 'pizzeria.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+#         'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'pizzeriadb',
